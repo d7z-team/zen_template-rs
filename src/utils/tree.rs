e18fa3ce36e81f91442cc49be672d3d21e4286a5
dragon <ExplodingFKL@gmail.com>
2023-01-21 23:33:45 +0800

    feat: 变更 tree

--- src/utils/tree.rs
+++ src/utils/tree.rs
@@ -1,12 +1,17 @@
 use std::cell::RefCell;
-use std::fmt::Debug;
-use std::mem;
+use std::fmt::{Debug, Formatter};
 use std::rc::{Rc, Weak};
+use std::slice::Iter;
 
-#[derive(Debug)]
 struct Tree<T: Debug + Sized> {
     pub root: Rc<RefCell<TreeEntity<T>>>,
+    pub cache: TreeCache<T>,
 }
+
+struct TreeCache<T: Debug + Sized> {
+    pub list: Vec<Weak<RefCell<TreeEntity<T>>>>,
+}
+
 
 #[derive(Debug)]
 struct TreeBuilder<T: Debug + Sized> {
@@ -23,14 +28,31 @@ struct TreeEntity<T: Debug + Sized> {
 }
 
 
-
 impl<T: Sized + Debug> Tree<T> {
     fn builder() -> TreeBuilder<T> {
-       TreeBuilder::new()
+        TreeBuilder::new()
     }
-
 }
 
+
+impl<T: Sized + Debug> Tree<T> {
+    fn iter(&self) -> Iter<'_, TreeEntity<T>> {
+        todo!()
+    }
+}
+
+impl<T: Sized + Debug> Debug for Tree<T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        todo!()
+    }
+}
+
+
+impl<T: Debug + Sized> TreeCache<T> {
+    fn new(root: &Rc<RefCell<TreeEntity<T>>>) -> TreeCache<T> {
+        todo!()
+    }
+}
 
 impl<T: Sized + Debug> TreeBuilder<T> {
     fn new() -> TreeBuilder<T> {
@@ -41,46 +63,81 @@ impl<T: Sized + Debug> TreeBuilder<T> {
             pointer: weak,
         }
     }
-    fn _set(&mut self, new_data: Option<T>) -> Option<T> {
-        let mut new_data = new_data;
-        if let Some(data) = self.pointer.upgrade() {
-            let mut ref_mut = data.borrow_mut();
-            mem::swap(&mut ref_mut.data, &mut new_data)
+    #[inline]
+    fn _unwrap_point<F>(&mut self, func: F) -> &mut TreeBuilder<T>
+        where F: FnOnce(&Rc<RefCell<TreeEntity<T>>>) -> () {
+        if let Some(entity) = &self.pointer.upgrade() {
+            func(entity)
+        } else {
+            panic!("此时不应该得到 None .")
         }
-        new_data
+        self
     }
-    fn remove(&mut self) -> Option<T> {
-        self._set(None)
+    /// add new child node`
+    fn push(&mut self, data: T) -> &mut TreeBuilder<T> {
+        self._unwrap_point(|entity| {
+            let mut ref_mut = entity.borrow_mut();
+            ref_mut.child.push(Rc::new(RefCell::new(
+                TreeEntity {
+                    data: Some(data),
+                    child: vec![],
+                    parent: Rc::downgrade(entity),
+                }
+            )))
+        })
     }
-    fn set(&mut self, new_data: T) -> Option<T> {
-        self._set(Some(new_data))
+    fn set(&mut self, data: T) -> &mut TreeBuilder<T> {
+        self._unwrap_point(|e| {
+            let mut ref_mut = e.borrow_mut();
+            ref_mut.data = Some(data)
+        })
     }
-    fn add(&mut self, data: T) -> &mut TreeBuilder<T> {
-        self._add(Some(data))
+    fn new_child(&mut self, data: T) -> &mut TreeBuilder<T> {
+        self.push(data);
+        self.pointer = Rc::downgrade(self.pointer.upgrade().unwrap().borrow().child.last().unwrap());
+        self
     }
-    fn _add(&mut self, data: Option<T>) -> &mut TreeBuilder<T> {
-        if let Some(item) = self.pointer.upgrade() {
-            let mut ref_mut = item.borrow_mut();
-            ref_mut.child.push(Rc::new(RefCell::new(TreeEntity {
-                data,
-                child: vec![],
-                parent: Rc::downgrade(&item),
-            })))
-        }
+    fn new_empty_child(&mut self) -> &mut TreeBuilder<T> {
+        self._unwrap_point(|entity| {
+            let mut ref_mut = entity.borrow_mut();
+            ref_mut.child.push(Rc::new(RefCell::new(
+                TreeEntity {
+                    data: None,
+                    child: vec![],
+                    parent: Rc::downgrade(entity),
+                }
+            )))
+        });
+        self.pointer = Rc::downgrade(self.pointer.upgrade().unwrap().borrow().child.last().unwrap());
         self
     }
 
-    fn new_empty_child(&mut self) -> &mut TreeBuilder<T> {
+    fn current_count(&mut self) -> usize {
+        self.pointer.upgrade().unwrap().borrow().child.len()
+    }
+
+    fn join_child(&mut self, index: usize) -> &mut TreeBuilder<T> {
+        self.pointer = Rc::downgrade(self.pointer.upgrade().unwrap().borrow().child.get(index).unwrap());
         self
     }
-    fn new_child(&mut self, data: T) -> &mut TreeBuilder<T> {
-        self.add(data);
-        if let Some(item) = self.pointer.upgrade() {
-            item.borrow_mut().child.last().unwrap();
-        }
+    fn end_child(&mut self) -> &mut TreeBuilder<T> {
+        self.pointer = Rc::downgrade(&self.pointer.upgrade().expect("已到达根节点").borrow().parent.upgrade().unwrap());
         self
+    }
+    fn remove_child(&mut self, index: usize) -> &mut TreeBuilder<T> {
+        self._unwrap_point(|e| {
+            e.borrow_mut().child.remove(index);
+        })
+    }
+    fn build(self) -> Tree<T> {
+        let cache = TreeCache::new(&self.root);
+        Tree {
+            root: self.root,
+            cache,
+        }
     }
 }
+
 #[cfg(test)]
 mod test {
     use std::fmt::Debug;
@@ -97,5 +154,10 @@ mod test {
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        let mut builder: TreeBuilder<&'static str> = Tree::builder();
+        builder.push("asas").new_empty_child();
+        let tree = builder.build();
+        println!("{:?}", tree);
+    }
 }
